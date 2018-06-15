35bd227439a025219c76901d3bc23d412b826b99
mohit101 <mohit101@users.noreply.github.com>
2018-06-15 23:12:59 +0530

    Update AbstractImplTest.java

--- TestSpring/src/main/java/com/mycompany/javaconcepts/AbstractImplTest.java
+++ TestSpring/src/main/java/com/mycompany/javaconcepts/AbstractImplTest.java
@@ -3,6 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
+
+/*
+ It provides a facility to write an algorithm independent of any specific type of data. 
+ It is gaurentee type safe.
+ Generic programming enables the programmer to create classes,interfaces and methods in which type of data is specified as a parameter. It provides a facility to write an algorithm independent of any specific type of data. Generics also provide type safety. Type safety means ensuring that an operation is being performed on the right type of data before executing that operation.
+
+Using Generics, it has become possible to create a single class ,interface or method that automatically works with all types of data(Integer, String, Float etc). It has expanded the ability to reuse the code safely and easily.
+
+/*
 package com.mycompany.javaconcepts;
 
 /**
